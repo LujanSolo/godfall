c12a56bde75ef8b273897cbaed3f17243df543bd
LujanSolo <lujansolo@proton.me>
2026-05-27 14:47:13 -0400

    Fix database URL variable name

--- app/database.py
+++ app/database.py
@@ -86,7 +86,7 @@ SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 # SQL is being generated — useful for
 # debugging, like switching on thermal vision.
 engine = create_engine(
-    DATABASE_URL,
+    SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},
     echo=False
 )
